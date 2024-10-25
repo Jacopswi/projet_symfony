471c2042747bf93e4313e809fd6315e1e61d8102
jacopswi54 <jacopswi54@gmail.com>
2024-10-25 07:54:57 +0000

    ajout form

---
+++ src/Form/ProductType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Image;
+use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('stock')
+            ->add('status')
+            ->add('Image', EntityType::class, [
+                'class' => Image::class,
+                'choice_label' => 'id',
+            ])
+            ->add('Category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('products', EntityType::class, [
+                'class' => Product::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
